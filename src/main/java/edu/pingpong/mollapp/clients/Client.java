b5a6b28067c71a930ba1f5858ab5ef90c1f597ef
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-04-28 07:11:04 +0200

    feat: :sparkles: Add administrator package with git merge and Client Class is done

--- src/main/java/edu/pingpong/mollapp/clients/Client.java
+++ src/main/java/edu/pingpong/mollapp/clients/Client.java
@@ -1,4 +1,17 @@
 package edu.pingpong.mollapp.clients;
 
+import edu.pingpong.mollapp.administrator.ProgrammerTasks;
+
 public class Client {
+
+    private ProgrammerTasks programmerTasks = null;
+
+    public void setProgrammerTasks(ProgrammerTasks programmerTasks) {
+        this.programmerTasks = programmerTasks;
+    }
+
+    public void sendRequest(String request) {
+        this.programmerTasks.executeTasks(request);
+    }
+
 }
